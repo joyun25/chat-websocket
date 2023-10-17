f4d21bc5c265160f95cc60e09848fb79179ea515
Joyun Hsiao 蕭若昀 <joyun.hsiao@u-media.com.tw>
2023-10-17 16:13:47 +0800

    add feature: send messages

--- public/main.js
+++ public/main.js
@@ -1,7 +1,31 @@
 const socket = io()
 
 const clientsTotal = document.getElementById('clients-total')
+const messageContainer = document.getElementById('message-container')
+const nameInput = document.getElementById('name-input')
+const messageForm = document.getElementById('message-form')
+const messageInput = document.getElementById('message-input')
+
+messageForm.addEventListener('submit', e => {
+  e.preventDefault()
+  sendMessage()
+})
 
 socket.on('clients-total', (data) => {
   clientsTotal.innerText = `Total Clients: ${data}`
+})
+
+function sendMessage() {
+  console.log(messageInput.value)
+  const data = {
+    name: nameInput.value,
+    message: messageInput.value,
+    dateTime: new Date()
+  }
+
+  socket.emit('message', data)
+}
+
+socket.on('chat-message', data => {
+  console.log(data)
 })
