a820c75a93c1b33f05260c178a59a568d07950bd
Joyun Hsiao 蕭若昀 <joyun.hsiao@u-media.com.tw>
2023-10-17 15:59:03 +0800

    add feature: clients total

--- app.js
+++ app.js
@@ -7,6 +7,19 @@ const io = require('socket.io')(server)
 
 app.use(express.static(path.join(__dirname, 'public')))
 
-io.on('connection', (socket) => {
+let socketsConnected = new Set()
+
+io.on('connection', onConnected)
+
+function onConnected(socket) {
   console.log(socket.id)
-})
+  socketsConnected.add(socket.id)
+
+  io.emit('clients-total', socketsConnected.size)
+
+  socket.on('disconnect', () => {
+    console.log('Socket disconnected', socket.id)
+    socketsConnected.delete(socket.id)
+    io.emit('clients-total', socketsConnected.size)
+  })
+}
