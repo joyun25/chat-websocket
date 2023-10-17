1d46b52922118c850d32aaeb3b9306f3c5e9b5da
Joyun Hsiao 蕭若昀 <joyun.hsiao@u-media.com.tw>
2023-10-17 16:50:40 +0800

    add feature: message sound

--- public/main.js
+++ public/main.js
@@ -6,6 +6,8 @@ const nameInput = document.getElementById('name-input')
 const messageForm = document.getElementById('message-form')
 const messageInput = document.getElementById('message-input')
 
+const messageTone = new Audio('/message-tone.mp3')
+
 messageForm.addEventListener('submit', e => {
   e.preventDefault()
   sendMessage()
@@ -31,6 +33,7 @@ function sendMessage() {
 
 socket.on('chat-message', data => {
   console.log(data)
+  messageTone.play()
   addMessageToUI(false, data)
 })
 
