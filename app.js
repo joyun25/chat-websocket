34965e226150e91e0d8b033b1b1764814f045fa5
Joyun Hsiao 蕭若昀 <joyun.hsiao@u-media.com.tw>
2023-10-17 15:40:48 +0800

    add socket.io and get socket id

--- app.js
+++ app.js
@@ -3,5 +3,10 @@ const path = require('path')
 const app = express()
 const PORT = process.env.PORT || 4000
 const server = app.listen(PORT, () => console.log(`chat server on port ${PORT}`))
+const io = require('socket.io')(server)
 
 app.use(express.static(path.join(__dirname, 'public')))
+
+io.on('connection', (socket) => {
+  console.log(socket.id)
+})
